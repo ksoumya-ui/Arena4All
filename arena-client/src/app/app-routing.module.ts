321e26e4d85ec1ce0f5751c8c581dee70cfd3c23
Sharma <amitsharma759@deloitte.com>
2022-09-24 11:05:42 +0530

    second page

--- arena-client/src/app/app-routing.module.ts
+++ arena-client/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "details", component: DetailsComponent },
+  { path: "details/:id", component: DetailsComponent },
   { path: "home", component: HomeComponent },
 ];
 
